172b191834f8fb2a63a23ebc1261a4c557ef5e16
JoanMendo <147327997+JoanMendo@users.noreply.github.com>
2024-01-08 12:41:35 +0100

    Add files via upload

---
+++ Refactoring/Refactoring/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Maths
+{
+    class MatematicalFunctions
+    {
+        static void Main(string[] args)
+        {
+           const string InitialMessage = "Escogeix una opció";
+            const string Options = "1. Nombre Senar\n2. Calcular Potència\n3. Valor Random\n4. Compat vocals o consonants d'un text\n5. Sortir";
+            const string ExitMessage = "Gràcies per utilitzar el programa";
+            const string Error = "Error, opció incorrecta";
+
+            Console.WriteLine(InitialMessage);
+            Console.WriteLine(Options);
+                                  
+        }
+    }
+}
